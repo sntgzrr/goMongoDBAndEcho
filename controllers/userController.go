b4512308089a1e12918fcd26b236b984550b3ebc
sntgzrr <santilozanoc077@gmail.com>
2024-03-21 18:29:56 -0500

    Middlewares added

--- controllers/userController.go
+++ controllers/userController.go
@@ -5,6 +5,7 @@ import (
 	"echoApiRest/services"
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"net/http"
 	"os"
@@ -19,62 +20,76 @@ func main() {
 	}
 	// Creating echo var
 	e := echo.New()
-
+	// Using group to create a new router group
+	g := e.Group("/api")
+	// This middleware logs the server interaction
+	g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "[method=${method}, host=${host}, uri=${uri}, status=${status}]\n",
+	}))
+	// This middleware causes there to be an authentication for use /api router
+	g.Use(middleware.BasicAuth(func(username string, password string, context echo.Context) (bool, error) {
+		if username == "santiago" && password == "root" {
+			return true, nil
+		}
+		return false, nil
+	}))
 	// GET Method for Users
-	e.GET("/api/users", func(c echo.Context) error {
-		// Getting Users
-		users, err := services.ReadService()
-		if err != nil {
-			return err
-		}
-		return c.JSON(http.StatusOK, users)
-	})
-
+	g.GET("/users", getUsers)
 	// POST Method for Users
-	e.POST("/api/users", func(c echo.Context) error {
-		var user models.User
-		var uid primitive.ObjectID
-
-		// Request Body into User Model
-		if err := c.Bind(&user); err != nil {
-			return err
-		}
-		// POST ID and CreatedAt
-		user.ID = uid
-		user.CreatedAt = time.Now()
-
-		// Creating User
-		if err := services.CreateService(user); err != nil {
-			return err
-		}
-		return c.String(http.StatusCreated, "User created successfully")
-	})
-
-	e.PUT("/api/users/:id", func(c echo.Context) error {
-		var user models.User
-		userID := c.Param("id")
-
-		// Request Body into User Model
-		if err := c.Bind(&user); err != nil {
-			return err
-		}
-
-		// Updating User
-		if err := services.UpdateService(user, userID); err != nil {
-			return err
-		}
-		return c.String(http.StatusOK, "User updated successfully")
-	})
-
-	e.DELETE("/api/users", func(c echo.Context) error {
-		userID := c.QueryParam("id")
-		if err := services.DeleteService(userID); err != nil {
-			return err
-		}
-		return c.String(http.StatusOK, "User deleted successfully")
-	})
-
+	g.POST("/users", postUser)
+	// PUT Method for Users
+	g.PUT("/users/:id", putUser)
+	// DELETE Method for Users
+	g.DELETE("/users", deleteUser)
 	// Starting Echo Server
 	e.Logger.Print(fmt.Sprintf("Listening port %s", port))
 	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
+}
+
+func getUsers(c echo.Context) error {
+	// Getting Users
+	users, err := services.ReadService()
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, users)
+}
+
+func postUser(c echo.Context) error {
+	var user models.User
+	var uid primitive.ObjectID
+	// Request Body into User Model
+	if err := c.Bind(&user); err != nil {
+		return err
+	}
+	// POST ID and CreatedAt
+	user.ID = uid
+	user.CreatedAt = time.Now()
+	// Creating User
+	if err := services.CreateService(user); err != nil {
+		return err
+	}
+	return c.String(http.StatusCreated, "User created successfully")
+}
+
+func putUser(c echo.Context) error {
+	var user models.User
+	userID := c.Param("id")
+	// Request Body into User Model
+	if err := c.Bind(&user); err != nil {
+		return err
+	}
+	// Updating User
+	if err := services.UpdateService(user, userID); err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, "User updated successfully")
+}
+
+func deleteUser(c echo.Context) error {
+	userID := c.QueryParam("id")
+	if err := services.DeleteService(userID); err != nil {
+		return err
+	}
+	return c.String(http.StatusOK, "User deleted successfully")
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.22
 require go.mongodb.org/mongo-driver v1.14.0
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/labstack/echo/v4 v4.11.4 // indirect
@@ -23,4 +24,5 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
